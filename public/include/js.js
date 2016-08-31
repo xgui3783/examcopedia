339f67d10f6a88ec5cda9810a28e7ca5c16f161f
Xiao Gui <xgui3783@gmail.com>
2016-08-31 14:55:52 +1000

    analytics implementation

--- public/include/js.js
+++ public/include/js.js
@@ -1,6 +1,15 @@
 /* id/class _ scope(name or core) _ type (btn/a etc) _ name */
 var socket = io();
 
+/* google analytics */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-83412525-1', 'auto');
+ga('send', 'pageview');
+
 $(document).ready(function(){
 	
 	$(document)
