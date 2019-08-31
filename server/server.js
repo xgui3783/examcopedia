435fe05bf82e37a8eaa334ac467d587014cf5ada
Xiao Gui <xgui3783@gmail.com>
2019-08-31 21:29:37 +0200

    bugfix: properly wait before querying db

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const app = require('./app/app')
 
 const PORT = process.env.PORT || 3001
 
+console.log(`waiting 5 sec before attempting to connecting`)
 setTimeout(() => {
+  const app = require('./app/app')
   app.listen(PORT, () => console.log(`app listening on port ${PORT}`))
 }, 5000)
